3213f41e9af491933a69fef5f84b66828fbe5dea
Bartek Banach <nano-stalker@wp.pl>
2017-11-03 22:50:52 +0100

    Add header bluring on scrolling

--- src/scripts/site/header/header.ts
+++ src/scripts/site/header/header.ts
@@ -1,5 +1,18 @@
-import { ScrollMagic } from "libs/ScrollMagic";
+import { ScrollMagic } from "../../libs/ScrollMagic";
+
+var welcomeImage: HTMLElement = <HTMLElement>document.querySelector("#welcome-section .cover");
+var welcomeHeader: HTMLElement = <HTMLElement>document.querySelector("#welcome-section .welcome-header"); 
+
+let controller: any = new ScrollMagic.Controller();
+let scene: any = new ScrollMagic.Scene({
+    triggerElement: "#welcome-section .welcome-image", 
+    loglevel: 3, duration: welcomeHeader.getBoundingClientRect().top});
+
+scene.addTo(controller)
+scene.on("progress", function (e: any) {
+    welcomeImage.style.filter = `blur(${5 * e.progress}px)`;
+});
 
 export function Init() {
-
+    console.log(welcomeHeader.getBoundingClientRect().top);
 }
