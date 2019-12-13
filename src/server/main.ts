cb79b63ca9e82dc403ba9a11358492197e447873
Bartek Banach <thebartekbanach.dev@gmail.com>
2019-12-13 08:46:01 +0100

    Add welcome background image cropper

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import addWelcomeBackgroundCropper from "./welcomeBackgroundCropper";
 
 const PORT = process.env.PORT || 8081;
 const app = express();
@@ -7,6 +8,9 @@ app.get("/", (_, res) => {
     res.send("Hello world!");
 });
 
+addWelcomeBackgroundCropper(app);
+
 app.listen(PORT).on("listening", () => {
+    // tslint:disable-next-line: no-console
     console.log("Server is listening on port " + PORT);
 });
---
+++ src/server/welcomeBackgroundCropper.ts
@@ -0,0 +1,68 @@
+import { Express } from "express";
+import { stat } from "fs";
+import { resolve } from "path";
+import sharp from "sharp";
+import { promisify } from "util";
+
+const statAsync = promisify(stat);
+
+const welcomeBackgroundPath = "dist/res/img/sections/welcome/background.jpg";
+const croppedWelcomeBackgroundPath = "dist/res/img/sections/welcome";
+
+const getCroppedWelcomeBackgroundPath =
+    (width: number, height: number) =>
+        `${croppedWelcomeBackgroundPath}/background.${width}x${height}.jpg`;
+
+async function getWelcomeImageSize(): Promise<{ width: number, height: number }> {
+    if ((getWelcomeImageSize as any).cached === undefined) {
+        const { width, height } = await sharp(welcomeBackgroundPath).metadata();
+        (getWelcomeImageSize as any).cached = { width, height };
+    }
+
+    return (getWelcomeImageSize as any).cached;
+}
+
+async function cropWelcomeTo(width: number, height: number) {
+    await sharp(welcomeBackgroundPath)
+        .resize(width, height)
+        .toFile(getCroppedWelcomeBackgroundPath(width, height));
+}
+
+async function fileExists(path: string) {
+    try {
+        await statAsync(path);
+        return true;
+    } catch (_) {
+        return false;
+    }
+}
+
+export default function init(app: Express) {
+    app.get("/welcome/:width/:height", async (req, res) => {
+        let width: number = null;
+        let height: number = null;
+
+        try {
+            width = parseInt(req.params.width, 10);
+            height = parseInt(req.params.height, 10);
+
+            if (width < 100 || height < 100) {
+                throw new Error("Size is too small");
+            }
+        } catch (err) {
+            res.sendStatus(400);
+            return;
+        }
+
+        const maxSize = await getWelcomeImageSize();
+
+        width = maxSize.width >= width ? width : maxSize.width;
+        height = maxSize.height >= height ? height : maxSize.height;
+
+        if (!await fileExists(getCroppedWelcomeBackgroundPath(width, height))) {
+            await cropWelcomeTo(width, height);
+        }
+
+        return res.sendFile(resolve(process.cwd(), getCroppedWelcomeBackgroundPath(width, height)));
+    });
+}
