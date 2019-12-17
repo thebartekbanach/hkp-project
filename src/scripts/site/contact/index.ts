2a3c094bfc01af9f266307a6b753c797897b7778
Bartek Banach <thebartekbanach.dev@gmail.com>
2019-12-17 13:43:45 +0100

    Add auto scroll to bottom when user expands location map and is not on the bottom of page.

--- src/scripts/site/contact/index.ts
+++ src/scripts/site/contact/index.ts
@@ -1,24 +1,52 @@
 import GoogleMapsLoader = require("google-maps");
 
-function expandMapElement(mapElement: Element) {
-    mapElement.classList.add("shown");
+function isPageScrolledBottom() {
+    return (window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 2;
+}
+
+function scrollToBottom(smooth: boolean = false) {
+    const body = document.body;
+    const html = document.documentElement;
+
+    const height = Math.max(
+        body.scrollHeight,
+        body.offsetHeight,
+        html.clientHeight,
+        html.scrollHeight,
+        html.offsetHeight
+    );
+
+    if (smooth) {
+        window.scrollTo({ left: 0, top: height, behavior: "smooth" });
+    } else {
+        window.scrollTo(0, height);
+    }
+}
+
+function scrollSmoothlyToBottomOfPage(): Promise<void> {
+    return new Promise((resolve) => {
+        let timeout = false;
+
+        const checkScroll = () => {
+            if (isPageScrolledBottom() || timeout) {
+                resolve();
+            } else {
+                setTimeout(checkScroll, 100);
+            }
+        };
+
+        scrollToBottom(true);
+        setTimeout(() => timeout = true, 1000);
+        checkScroll();
+    });
+}
+
+async function expandMapElement(mapElement: Element) {
+    if (!isPageScrolledBottom()) {
+        await scrollSmoothlyToBottomOfPage();
+    }
 
     let timeout = false;
-
-    const scrollToBottom = () => {
-        const body = document.body;
-        const html = document.documentElement;
-
-        const height = Math.max(
-            body.scrollHeight,
-            body.offsetHeight,
-            html.clientHeight,
-            html.scrollHeight,
-            html.offsetHeight
-        );
-
-        window.scrollTo(0, height);
-    };
 
     const updateAnimation = () => {
         scrollToBottom();
@@ -28,6 +56,7 @@ function expandMapElement(mapElement: Element) {
         }
     };
 
+    mapElement.classList.add("shown");
     setTimeout(() => timeout = true, 1000);
     updateAnimation();
 }
